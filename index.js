38e4c3de7c3707e8b6bb5c2bf26c473ce81dce76
Scott Ellis <sje397@gmail.com>
2021-01-15 22:21:23 +1100

    Don't print weird function text, add personal scopes

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 const { Client } = require('discord.js');
 const { token } = require('./settings');
-const math = require('mathjs');
 const https = require('https');
+
+// make eval more secure
+const { create, all } = require('mathjs');
+const math = create(all);
+
+const limitedEvaluate = math.evaluate
+
+math.import({
+  import: function () { throw new Error('Function import is disabled') },
+  createUnit: function () { throw new Error('Function createUnit is disabled') },
+  evaluate: function () { throw new Error('Function evaluate is disabled') },
+  parse: function () { throw new Error('Function parse is disabled') },
+  simplify: function () { throw new Error('Function simplify is disabled') },
+  derivative: function () { throw new Error('Function derivative is disabled') }
+}, { override: true })
+
+let scopes = {};
 
 const client = new Client();
 
@@ -10,10 +26,25 @@ client.on('ready', () => console.log('Ready!'));
 client.on('message', (msg) => {
   if (msg.author.bot) return;
 
+  let forget = msg.content.toLowerCase() === "calcbot forget";
+
+  if(!scopes[msg.author.id] || forget) {
+    scopes[msg.author.id] = {};
+  }
+
+  if(forget) {
+    msg.channel.send("Scope cleared");
+    return;
+  }
+
   try {
-    let response = math.evaluate(msg.content).toString();
-    if(response && response.length > 0 && response != msg.content && '"' + response + '"' != msg.content) {
-      msg.channel.send(response);
+    let response = limitedEvaluate(msg.content, scopes[msg.author.id]);
+    if(response.toString() !== msg.content
+      && typeof response !== 'function' 
+      && '"' + response + '"' !== msg.content
+      && "'" + response + "'" !== msg.content)
+    {
+      msg.channel.send(response.toString());
     }
   }
   catch (err) {
